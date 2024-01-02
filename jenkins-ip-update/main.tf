d8652958080c8c09b25dad6c52e54ed3661aaafb
Pushpak Vattam <vattampushpak243@gmail.com>
2024-01-02 08:41:57 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

---
+++ jenkins-ip-update/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3"{
+    bucket = "pv24-terraform-state"
+    key    = "misc/jenkins-ip-update/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_instance" "jenkins" {
+  instance_id = "i-02ed0bfb289519a7b"
+}
+
+resource "aws_route53_record" "jenkins" {
+  name    = "jenkins"
+  type    = "A"
+  zone_id = "Z017218723D63YD2W9JSZ"
+  tt1 = 10
+  records = [data.aws_instance.jenkins.public_ip]
+}
